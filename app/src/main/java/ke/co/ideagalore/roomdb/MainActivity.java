31724220632974ddeb8cbb999c6d1829cac3cf04
w.kariukijames <kwjames9326@gmail.com>
2022-02-28 15:54:43 +0300

    UI Updates

--- app/src/main/java/ke/co/ideagalore/roomdb/MainActivity.java
+++ app/src/main/java/ke/co/ideagalore/roomdb/MainActivity.java
@@ -1,14 +1,14 @@
 package ke.co.ideagalore.roomdb;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -22,30 +22,54 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        fab=findViewById(R.id.fab);
+        fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Dialog dialog=new Dialog(view.getContext());
+                Dialog dialog = new Dialog(view.getContext());
                 dialog.setContentView(R.layout.user_dialog);
                 //Initialize the width and height
-                int width= WindowManager.LayoutParams.MATCH_PARENT;
-                int height=WindowManager.LayoutParams.WRAP_CONTENT;
+                int width = WindowManager.LayoutParams.MATCH_PARENT;
+                int height = WindowManager.LayoutParams.WRAP_CONTENT;
                 //Set layout
                 dialog.getWindow().setLayout(width, height);
                 //Show the dialog
                 dialog.show();
 
-                EditText name, designation,organisation,city, email, officeLine, mobileLine, fax;
-                name=dialog.findViewById(R.id.edt_name);
-                designation=dialog.findViewById(R.id.edt_designation);
-                organisation=dialog.findViewById(R.id.edt_organisation);
-                city=dialog.findViewById(R.id.edt_city);
-                email=dialog.findViewById(R.id.edt_email);
-                officeLine=dialog.findViewById(R.id.edt_direct_line);
-                mobileLine=dialog.findViewById(R.id.edt_mobile_line);
-                fax=dialog.findViewById(R.id.edt_fax);
-                Button saveCard=dialog.findViewById(R.id.btn_save);
+                EditText name;
+                EditText designation;
+                EditText organisation;
+                EditText city;
+                EditText email;
+                EditText officeLine;
+                EditText mobileLine;
+                EditText fax;
+                name = dialog.findViewById(R.id.edt_name);
+                designation = dialog.findViewById(R.id.edt_designation);
+                organisation = dialog.findViewById(R.id.edt_organisation);
+                city = dialog.findViewById(R.id.edt_city);
+                email = dialog.findViewById(R.id.edt_email);
+                officeLine = dialog.findViewById(R.id.edt_direct_line);
+                mobileLine = dialog.findViewById(R.id.edt_mobile_line);
+                fax = dialog.findViewById(R.id.edt_fax);
+                Button saveCard = dialog.findViewById(R.id.btn_save);
+
+                saveCard.setOnClickListener(view1 -> {
+                    //Validat fields then Save new user card
+
+                    String uName, uDesignation, uOrganisation, uCity, uEmail;
+                    double uOfficeLine, uMobileLine, uFax;
+
+                    uName=name.getText().toString().trim();
+                    uDesignation=designation.getText().toString().trim();
+                    uOrganisation=organisation.getText().toString().trim();
+                    uCity=city.getText().toString().trim();
+                    uEmail=email.getText().toString().toLowerCase().trim();
+                    uOfficeLine=Double.parseDouble(officeLine.getText().toString().trim());
+                    uMobileLine=Double.parseDouble(mobileLine.getText().toString().toString());
+                    uFax=Double.parseDouble(fax.getText().toString().trim());
+
+                });
             }
         });
     }
