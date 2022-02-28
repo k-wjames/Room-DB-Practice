1446cff6d9879cbaff4baf981c216ea05d5dc679
w.kariukijames <kwjames9326@gmail.com>
2022-02-28 15:22:43 +0300

    UI Updates

--- app/src/main/java/ke/co/ideagalore/roomdb/MainActivity.java
+++ app/src/main/java/ke/co/ideagalore/roomdb/MainActivity.java
@@ -1,14 +1,31 @@
 package ke.co.ideagalore.roomdb;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton fab;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fab=findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Dialog dialog=new Dialog(view.getContext());
+                dialog.setContentView(R.layout.user_dialog);
+            }
+        });
     }
 }
